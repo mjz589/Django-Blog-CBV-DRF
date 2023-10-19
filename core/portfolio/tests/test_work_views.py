eec62f6b5c18026d24d3989bbac969fa9e10e6f5
Mohamad Javad <mjz589.2018@gmail.com>
2023-10-19 12:02:09 +0330

    refactory 1.1

--- core/portfolio/tests/test_work_views.py
+++ core/portfolio/tests/test_work_views.py
@@ -13,6 +13,7 @@ def api_client():
     client = APIClient()
     return client
 
+
 @pytest.fixture
 def common_user():
     user = User.objects.create_user(
@@ -53,7 +54,7 @@ def create_work(staff_user):
     profile.save()
     cat_list = []
     for name in category_list:
-        category, _ =PortfolioCategory.objects.get_or_create(
+        category, _ = PortfolioCategory.objects.get_or_create(
             name=name,
         )
         cat_list.append(category)
